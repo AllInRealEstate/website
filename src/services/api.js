62eed8af3e149a07c6ea4d8d15a45c33948a937e
Majd Salameh <majdsalameh9@gmail.com>
2026-03-08 23:56:57 +0200

    test hardcoded url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
 //  
-const API_BASE_URL =import.meta.env.VITE_API_URL || 'http://localhost:5000/api'; 
+//const API_BASE_URL =import.meta.env.VITE_API_URL || 'http://localhost:5000/api'; 
+const API_BASE_URL = 'https://backend-production-4a23.up.railway.app/api';
 
 // 🆕 Create a completely clean instance for public, non-protected endpoints
 export const publicApi = axios.create({
