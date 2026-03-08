485606070a10274f08d56c0faa79b1d6c4a48b44
Majd Salameh <majdsalameh9@gmail.com>
2026-03-09 00:02:53 +0200

    switching to dynamic url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
-//  
-//const API_BASE_URL =import.meta.env.VITE_API_URL || 'http://localhost:5000/api'; 
-const API_BASE_URL = 'https://backend-production-4a23.up.railway.app/api';
+
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 // 🆕 Create a completely clean instance for public, non-protected endpoints
 export const publicApi = axios.create({
